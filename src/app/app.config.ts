bcafa80337ab76feb7f3ced75263bbb1e5b23df2
Camila <ca229863@fi365.ort.edu.uy>
2026-04-26 20:42:36 -0300

    configuracion de primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
 
@@ -8,6 +10,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    providePrimeNG({ theme: { preset: Aura } })
   ]
 };
