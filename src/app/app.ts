bd0045a882f05aee2ebb2547e71fcb93997552ac
Camila <ca229863@fi365.ort.edu.uy>
2026-04-28 19:08:00 -0300

    fixes de estilo y detalles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/layout/header/header';
 import { Footer } from './shared/layout/footer/footer';
 
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,Footer],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
- 
-}
+export class App {}
--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-header',
-  imports: [RouterLink,RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
