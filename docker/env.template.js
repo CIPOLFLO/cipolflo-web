65d68488d7d5babf10952a805f7f126b48791132
Camila <ca229863@fi365.ort.edu.uy>
2026-06-06 20:29:11 -0300

    configuraciones de docker

---
+++ docker/env.template.js
@@ -0,0 +1,8 @@
+(function (window) {
+  window.__env = {
+    apiUrl: '/api/v1',
+    auth0Domain: '${AUTH0_DOMAIN}',
+    auth0ClientId: '${AUTH0_CLIENT_ID}',
+    auth0Audience: '${AUTH0_AUDIENCE}',
+  };
+}(this));
